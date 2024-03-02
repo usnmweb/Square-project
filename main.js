9ad7fc0f2660345d4469ac9f0c29c38c2cea4e48
Alberto Barbagallo <128221024+usnmweb@users.noreply.github.com>
2024-03-02 17:42:55 +0100

    styling navbar

--- main.js
+++ main.js
@@ -12,12 +12,23 @@ for (let i = 0; i < button.length; i++) {
   button[i].style.setProperty("--main-bg-color", color);
 }
 
+let counter = 0;
+
 function addHTML(element) {
   let newHTML = document.createElement("div");
   newHTML.innerHTML =
     '<div class="q"><button class="riquadro" onclick="addHTML(this)"></button><button class="riquadro" onclick="addHTML(this)"></button><button class="riquadro" onclick="addHTML(this)"></button><button class="riquadro" onclick="addHTML(this)"></button></div>';
 
   let button = newHTML.getElementsByTagName("button");
+
+  function updateScore() {
+    let scoreElememt = document.getElementById("score");
+    scoreElememt.textContent = counter;
+  }
+
+  counter = counter + 1;
+  console.log(counter);
+  updateScore();
 
   for (let i = 0; i < button.length; i++) {
     let color = createRandomColor();
